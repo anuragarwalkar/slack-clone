c0579f64ae8e51d7324c5459dbe42420a44c3d66
Anurag Arwalkar <anurag.arwalkar@globant.com>
2022-02-14 11:44:57 +0530

    added basic header icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import "./app.styles.scss";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HeaderComponent from "./components/header/header.component";
 
-const HomePage = () => <h1>this is homepage</h1>;
+const HomePage = () => {
+  return <HeaderComponent></HeaderComponent>;
+};
 
 function App() {
   return (
---
+++ src/components/header/header.component.tsx
@@ -0,0 +1,80 @@
+import React from "react";
+import styled from "styled-components";
+import Avatar from "@mui/material/Avatar";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import SearchIcon from "@mui/icons-material/Search";
+import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
+
+function HeaderComponent() {
+  return (
+    <HeaderContainer>
+      <HeaderLeft>
+        <HeaderAvatar />
+        <AccessTimeIcon />
+      </HeaderLeft>
+
+      <HeaderSearch>
+        <SearchIcon></SearchIcon>
+        <input type="text" placeholder="Search" />
+      </HeaderSearch>
+      <HeaderRight>
+        <HelpOutlineOutlinedIcon />
+      </HeaderRight>
+    </HeaderContainer>
+  );
+}
+
+export default HeaderComponent;
+
+const HeaderSearch = styled.div`
+  flex: 0.4;
+  opacity: 1;
+  border-radius: 6px;
+  text-align: center;
+  display: flex;
+  padding: 0 50px;
+  color: grey;
+  border: 1px grey solid;
+
+  > input {
+    background-color: transparent;
+    border: none;
+    text-align: center;
+    min-width: 30vw;
+    outline: none;
+    color: white;
+  }
+`;
+
+const HeaderContainer = styled.div`
+  display: flex;
+  position: fixed;
+  width: 100%;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px 0;
+  background-color: #3f0f40;
+  color: white;
+`;
+
+const HeaderLeft = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: 20px;
+  flex: 0.3;
+
+  > .MuiSvgIcon-root {
+    margin-left: auto;
+    margin-right: 30px;
+  }
+`;
+
+const HeaderRight = styled.div``;
+
+const HeaderAvatar = styled(Avatar)`
+  cursor: pointer;
+
+  :hover {
+    opacity: 0.8;
+  }
+`;
